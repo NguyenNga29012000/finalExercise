6e94cfb872080796010a0681b1f3669338206293
ndt-0152 <duct9888@gmail.com>
2021-04-09 22:09:48 +0700

    'client-server'

---
+++ chat_application-main/src/App.js
@@ -0,0 +1,25 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = '5ddadd7e-0160-4c83-97e5-bef1bb9a6b2c';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
+  );
+};
+
+// infinite scroll, logout, more customizations...
+
+export default App;
